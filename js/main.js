62d54d587eeda0dafdc9deab44513fad101eb853
carola <carolacancinoc@gmail.com>
2016-11-07 12:48:31 -0300

    avances

--- js/main.js
+++ js/main.js
@@ -1,2 +1,11 @@
-
-
+function añadirTarea(){
+	
+	var contenido=document.getElementById('auto').value;
+	var div1=document.createElement('div');
+	div1.classList.add("row");
+	
+	
+	var checkIn=document.createElement('input');
+	
+	checkIn.setAttribute("type", "checkbox");
+	checkIn.classList.add("col-xs-1","col-sm-1","col-md-1");
